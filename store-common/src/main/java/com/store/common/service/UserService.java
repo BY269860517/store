2fb92deae37b71724fb7533a82e7137f3fe2e39d
BY269860517 <38999493+BY269860517@users.noreply.github.com>
2019-07-21 18:37:30 +0800

    是

--- store-common/src/main/java/com/store/common/service/UserService.java
+++ store-common/src/main/java/com/store/common/service/UserService.java
@@ -1,7 +1,12 @@
 package com.store.common.service;
 
 import com.store.common.pojo.User;
-
+/**
+* @Description: 用户接口
+* @author: panboyang
+* @date :2019-07-21 18:36:42
+* @params: No such property: code for class: Script1
+ */
 public interface UserService {
     User getUser(User user);
 }
